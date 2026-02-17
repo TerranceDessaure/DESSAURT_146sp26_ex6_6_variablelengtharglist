3e640ec1575ca5aa8667d8870fdc83ffab01f3e4
Terrance Dessaure <dessaurt@email.sc.edu>
2026-02-16 22:57:54 -0500

    switched code to the correct application

--- src/varibleLengthArgList.java
+++ src/varibleLengthArgList.java
@@ -1,7 +1,31 @@
 /**
- *
- * @author terrancedessaure
+ * This program calculates the of a set of integers
+ * that uses a method named `product` that will use a 
+ * a variable-length argument list
+ * 
+ * @author DESSAURT@email.sc.edu
  */
 public class varibleLengthArgList {
-    
-}
+    public static void main(String[] args) {
+        /* INITIALIZING PHASE */
+        
+        // Initialize the 20-element array
+        
+        /* PROCESSING PHASE */
+        
+        // Use a for loop to loop throught the each iteration
+        // of the array list and assgin a random number for
+        // each value
+        
+        // Determine what is the smallest number and assign 
+        // that to its  own variable.
+        
+        // Determine what is the largest number and assign
+        // to its own variable.
+        
+        /* TERMINATION PHASE */
+        
+        // display the out put
+        
+    } // end method main
+}//end method variableLegnthArgList
